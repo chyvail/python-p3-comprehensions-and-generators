43e8b8edbf7d57055424be31ef10da9eba9925aa
joshua chivile <joshuachivile@gmail.com>
2023-11-26 18:22:46 +0300

    List Comprehension in Python

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list_items = [i for i in num_list if(i%2 == 0)]
+    return list_items
+
+# return_evens([0, 1, 3, 5, 7, 8, 9])
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = [i+"!" for i in sentence_list]
+    return exclamation
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
